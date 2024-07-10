8c6903b2b3eedc5472b82d6bce66172c07aca576
lcs <lcs@pm.me>
2024-07-09 18:35:09 -0700

    Set up stub testing package for dice.go

--- cmd/scraper/dice_test.go
+++ cmd/scraper/dice_test.go
@@ -1,1 +1,14 @@
 package cmd
+
+import (
+  "testing"
+  "github.com/stretchr/testify/assert"
+)
+
+func TestScrape(t *testing.T) {
+	// assert equality
+  assert.Equal(t, 123, 123, "they should be equal")
+
+  // assert inequality
+  assert.NotEqual(t, 123, 456, "they should not be equal")
+}
