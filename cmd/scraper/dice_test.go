4e0a18f713184bfc93a9bbbed27125870dae3255
lcs <lcs@pm.me>
2024-07-09 22:45:27 -0700

    Implemented test for getting dice query from Filter struct

--- cmd/scraper/dice_test.go
+++ cmd/scraper/dice_test.go
@@ -1,26 +1,33 @@
 package cmd
 
 import (
-  "testing"
-  "github.com/stretchr/testify/assert"
-)
-
-
+	"github.com/lcsebastian/urban-palm-tree/cmd"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"fmt"
+)	
 
 func TestScrape(t *testing.T) {
 	// assert equality
-  assert.Equal(t, 123, 123, "they should be equal")
+	assert.Equal(t, 123, 123, "they should be equal")
 
-  // assert inequality
-  assert.NotEqual(t, 123, 456, "they should not be equal")
+	// assert inequality
+	assert.NotEqual(t, 123, 456, "they should not be equal")
 }
 
 func TestGetQuery(t *testing.T) {
-	filter := Filter {
-		1,
-		"TODAY",
-		true,
-		["Los Angeles, CA, USA"],
-		
+	filter := cmd.LoadTestFilters("../../data/filter.json")[0]
+	assert.NotNil(t, filter)
+
+	expectedQuery := DiceQuery{
+		[]string{"software engineer", "java", "full stack", "gcp", "jenkins"},
+		"Los Angeles, CA, USA",
+		[]string{"Remote"},
+		[]string{"FULLTIME", "CONTRACTS"},
+		"ONE",
 	}
+	actualQuery := getQuery(filter)
+	assert.NotNil(t, actualQuery)
+	assert.Equal(t, expectedQuery, actualQuery)
+	fmt.Println("Stringer'd query : ", actualQuery)
 }
