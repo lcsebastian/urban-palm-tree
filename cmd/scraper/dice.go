8c006f02340c18ef9cfde3f9c091b84cd2f68a76
lcs <lcs@pm.me>
2024-07-09 20:19:53 -0700

    added some stuf code for testing'added colly stub code

--- cmd/scraper/dice.go
+++ cmd/scraper/dice.go
@@ -1,42 +1,47 @@
 package cmd
 
 import (
-//	"github.com/gocolly/colly"
-"github.com/lcsebastian/urban-palm-tree/cmd"
+	"fmt"
+	"github.com/gocolly/colly"
+	"github.com/lcsebastian/urban-palm-tree/cmd"
 )
 
 type Filter cmd.Filter
-
-type SalaryRange struct {
-	Min float64
-	Max float64
-}
-
-type JobType int
-
-const (
-	FullTime JobType = iota
-	PartTime
-	Contract
-)
-
-type Job struct {
-	Link        string
-	Title       string
-	Description string
-	Company     string
-	Location    string
-	Remote      bool
-	Salary      SalaryRange
-	Type        JobType
-	PostedDate  string
-	Skills      []string
-}
+type Job cmd.Job
 
 const (
 	baseUrl = "https://dice.com"
 )
 
+// getQuery converts a Filter struct into a query string for dice.com.
+// If the conversion is successful the string is returned, else nil is returned.
+func getQuery(filter *Filter) string {
+	return "I'm a stub"
+}
+
 func Scrape(filter Filter) []Job {
+	// Instantiate default collector
+	c := colly.NewCollector(
+		colly.AllowedDomains("dice.com", "www.dice.com"),
+	)
+
+	// On every a element which has href attribute call callback
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		// Print link
+		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+		// Visit link found on page
+		// Only those links are visited which are in AllowedDomains
+		c.Visit(e.Request.AbsoluteURL(link))
+	})
+
+	// Before making a request print "Visiting ..."
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+	
+	
+	c.Visit(baseUrl)
+
 	return nil
 }
--- cmd/scraper/dice_test.go
+++ cmd/scraper/dice_test.go
@@ -5,10 +5,22 @@ import (
   "github.com/stretchr/testify/assert"
 )
 
+
+
 func TestScrape(t *testing.T) {
 	// assert equality
   assert.Equal(t, 123, 123, "they should be equal")
 
   // assert inequality
   assert.NotEqual(t, 123, 456, "they should not be equal")
+}
+
+func TestGetQuery(t *testing.T) {
+	filter := Filter {
+		1,
+		"TODAY",
+		true,
+		["Los Angeles, CA, USA"],
+		
+	}
 }
