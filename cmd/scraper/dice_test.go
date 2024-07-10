da9a2cfeadd102a3db4841ae91dbe744debcd449
lcs <lcs@pm.me>
2024-07-10 01:20:48 -0700

    actually calling Scrape function in test file

--- cmd/scraper/dice_test.go
+++ cmd/scraper/dice_test.go
@@ -9,11 +9,9 @@ import (
 )
 
 func TestScrape(t *testing.T) {
-	// assert equality
-	assert.Equal(t, 123, 123, "they should be equal")
-
-	// assert inequality
-	assert.NotEqual(t, 123, 456, "they should not be equal")
+	filter := cmd.LoadTestFilters("../../data/filter.json")[0]
+	assert.NotNil(t, filter)
+	Scrape(filter)
 }
 
 func TestGetQuery(t *testing.T) {
