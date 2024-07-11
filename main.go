387ea1fa264ac37be69207df743517234b918e90
lcs <lcs@pm.me>
2024-07-10 18:33:38 -0700

    added route for GET jobs

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 	"github.com/lcsebastian/urban-palm-tree/cmd"
+	scraper "github.com/lcsebastian/urban-palm-tree/cmd/scraper"
 )
 
 func getResumes(c *gin.Context) {
@@ -15,10 +17,16 @@ func getFilters(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, cmd.LoadTestFilters("data/filter.json"))
 }
 
+func getJobs(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, scraper.CollyScrape())
+}
+
 func main() {
+
 	fmt.Println("Setting up router")
 	router := gin.Default()
 	router.GET("/resumes", getResumes)
 	router.GET("/filters", getFilters)
+	router.GET("/jobs", getJobs)
 	router.Run("localhost:8080")
 }
